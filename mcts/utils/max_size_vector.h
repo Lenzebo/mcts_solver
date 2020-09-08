b5e7fce1eaca413ecd8c45e50f039ee5330d709a
David Lenz <LenzDavid@gmx.de>
2020-09-08 08:24:44 +0200

    Add constexpr to max_size_vector functions

--- mcts/utils/max_size_vector.h
+++ mcts/utils/max_size_vector.h
@@ -9,39 +9,42 @@ class MaxSizeVector
 {
   public:
     MaxSizeVector() = default;
-    MaxSizeVector(const MaxSizeVector& other) : data_(other.data_), count_(other.count_) {}
+    constexpr MaxSizeVector(const MaxSizeVector& other) : data_(other.data_), count_(other.count_) {}
     MaxSizeVector(const std::vector<T>& other) { insert(begin(), other.begin(), other.end()); }
-    MaxSizeVector(MaxSizeVector&& other) noexcept : data_(std::move(other.data_)), count_(std::move(other.count_)) {}
+    constexpr MaxSizeVector(MaxSizeVector&& other) noexcept
+        : data_(std::move(other.data_)), count_(std::move(other.count_))
+    {
+    }
 
-    MaxSizeVector& operator=(const MaxSizeVector& other)
+    constexpr MaxSizeVector& operator=(const MaxSizeVector& other)
     {
         data_ = other.data_;
         count_ = other.count_;
         return *this;
     }
 
-    MaxSizeVector& operator=(MaxSizeVector&& other) noexcept
+    constexpr MaxSizeVector& operator=(MaxSizeVector&& other) noexcept
     {
         data_ = std::move(other.data_);
         count_ = std::move(other.count_);
         return *this;
     }
 
-    [[nodiscard]] T* begin() { return data_.begin(); }
-    [[nodiscard]] T* end() { return begin() + count_; }
-    [[nodiscard]] const T* begin() const { return data_.begin(); }
-    [[nodiscard]] const T* end() const { return begin() + count_; }
+    [[nodiscard]] constexpr T* begin() { return data_.begin(); }
+    [[nodiscard]] constexpr T* end() { return begin() + count_; }
+    [[nodiscard]] constexpr const T* begin() const { return data_.begin(); }
+    [[nodiscard]] constexpr const T* end() const { return begin() + count_; }
 
-    void reserve(size_t newSize)
+    constexpr void reserve(size_t newSize)
     {
         assert(newSize < MAX_SIZE);
         (void)newSize;
     }
 
-    void clear() { count_ = 0; }
+    constexpr void clear() { count_ = 0; }
 
     template <class InputIterator>
-    T* insert(T* position, InputIterator first, InputIterator last)
+    constexpr T* insert(T* position, InputIterator first, InputIterator last)
     {
         auto dist = std::distance(first, last);
         assert(size() + dist <= capacity());
@@ -56,11 +59,11 @@ class MaxSizeVector
         return position;
     }
 
-    [[nodiscard]] bool empty() const { return count_ == 0; }
-    [[nodiscard]] size_t size() const { return count_; }
-    [[nodiscard]] size_t capacity() const { return MAX_SIZE; }
+    [[nodiscard]] constexpr bool empty() const { return count_ == 0; }
+    [[nodiscard]] constexpr size_t size() const { return count_; }
+    [[nodiscard]] constexpr size_t capacity() const { return MAX_SIZE; }
 
-    void erase(T* elem)
+    constexpr void erase(T* elem)
     {
         assert(elem != end());
 
@@ -74,28 +77,28 @@ class MaxSizeVector
         count_--;
     }
 
-    T& operator[](size_t idx)
+    constexpr T& operator[](size_t idx)
     {
         assert(idx < count_);
         return data_[idx];
     }
 
-    const T& operator[](size_t idx) const
+    constexpr const T& operator[](size_t idx) const
     {
         assert(idx < count_);
         return data_[idx];
     }
 
-    const T& back() const { return (*this)[count_ - 1]; }
-    const T& front() const { return (*this)[0]; }
+    constexpr const T& back() const { return (*this)[count_ - 1]; }
+    constexpr const T& front() const { return (*this)[0]; }
 
-    void push_back(T&& elem)
+    constexpr void push_back(T&& elem)
     {
         assert(count_ < MAX_SIZE);
         data_[count_] = elem;
         count_++;
     }
-    void push_back(const T& elem)
+    constexpr void push_back(const T& elem)
     {
         assert(count_ < MAX_SIZE);
         data_[count_] = elem;
@@ -106,4 +109,4 @@ class MaxSizeVector
     std::array<T, MAX_SIZE> data_{};
     size_t count_ = 0;
 };
-}
+}  // namespace mcts
